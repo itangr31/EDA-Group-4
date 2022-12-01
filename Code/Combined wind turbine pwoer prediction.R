66c2e2b8540ffddbcd2b428ef9bd0e15fe0f0a1d
itangr31 <rizaldi.muhammad.ilham@gmail.com>
2022-12-01 11:17:57 +0000

    add dataset

--- Code/Combined wind turbine pwoer prediction.R
+++ Code/Combined wind turbine pwoer prediction.R
@@ -28,6 +28,8 @@ Turbine2019 <- read.csv("~/University/5 EDA/Coursework/Wind turbine power predic
 Turbine2020 <- read.csv("~/University/5 EDA/Coursework/Wind turbine power prediction/Turbine_Data_Penmanshiel_01_2020-01-01_-_2021-01-01_1042.csv")
 Turbine2021 <- read.csv("~/University/5 EDA/Coursework/Wind turbine power prediction/Turbine_Data_Penmanshiel_01_2021-01-01_-_2021-07-01_1042.csv")
 
+Dataset
+
 #Combine all datasets to get final time series data
 Turbine<- rbind(Turbine2016, Turbine2017, Turbine2018, Turbine2019, Turbine2020, Turbine2021)
 Turbine<- rbind(Turbine2020, Turbine2021)
